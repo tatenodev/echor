65eed28009c023ee0b2ebe0e663d86244fd9f3ad
tatenodev <gd28.c.tateno@gmail.com>
2024-01-27 18:02:57 +0900

    テスト追加

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,6 @@
-use assert_cmd::{Command, output::OutputOkExt};
+use assert_cmd::Command;
 use predicates::prelude::*;
-use std::{fs, vec, ffi::{OsStr, OsString}};
+use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -13,33 +13,32 @@ fn dies_no_args() -> TestResult {
   Ok(())
 }
 
-#[test]
-fn hello1() -> TestResult {
-  let outfile = "tests/expected/hello1.txt";
-  let expected = fs::read_to_string(outfile)?;
-  let mut cmd = Command::cargo_bin("echor")?;
-  cmd.arg("Hello there").assert().success().stdout(expected + "\n");
-  Ok(())
-}
-
-#[test]
-fn hello2() -> TestResult {
-  let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-  let mut cmd = Command::cargo_bin("echor")?;
-  cmd.args(vec!["Hello", "there"])
-    .assert()
-    .success()
-    .stdout(expected);
-  Ok(())
-}
-
-
 fn run(args: &[&str], expected_file: &str) -> TestResult {
   let expected = fs::read_to_string(expected_file)?;
   Command::cargo_bin("echor")?
     .args(args)
     .assert()
     .success()
-    .stdout(expected);
+    .stdout(expected + "\n");
   Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+  run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+  run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+  run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+  run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
